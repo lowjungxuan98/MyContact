ecb4fbbb3b7f0ce1dd60dcf2a231ccf9d587d9c6
lowjungxuan <lowjungxuan@gmail.com>
2024-08-08 13:12:08 +0800

    Initial Commit

---
+++ MyContact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyContact
+//
+//  Created by Low Jung Xuan on 08/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
